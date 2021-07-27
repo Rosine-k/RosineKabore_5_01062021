db11bfae8ec96a752757a25aa96e78edbc40135c
Rosine <kabore.rosine75@gmail.com>
2021-07-27 11:29:56 +0200

    affichage produit et repertoire

--- js/produit.js
+++ js/produit.js
@@ -1,26 +1,20 @@
-const createProduct = (data) =>{
-  data.price = data.price / 100;
+(async function() {
+  const cameraId = getCameraId()
+  const camera = await getCamera(cameraId)
+  hydrateCamera(camera)
+})()
 
-  let price = priceFormat(data.price);
-
-  document.getElementById('card-produit').style.visibility = 'visible';
-  
+function getCameraId() {
+  return new URL(location.href).searchParams.get("id")
 }
 
-const params = new URLSearchParams(window.location.search);
-let idProduct = params.get('idProduct');
-
-let url = "http://localhost:3000/api/cameras/" + idProduct;
-
-fetch(url)
-  .then(response => response.json())
-  .then(response => {
-     console.log(response);
-     let product = createProduct(response);
-     console.log(product);
-     
-
-     document.querySelector(".card-produit") .innerHTML +=    `<div class="col-sm-8">
+function getCameraId(cameraId) {
+  return fetch(`http://localhost:3000/api/cameras/${cameraId}`)
+   .then(data => data.json())
+   .then(jsonListCamera => {
+     for(let jsonCamera of jsonListCamera){
+       let camera = new Camera(jsonCamera);
+       document.querySelector(".card-produit") .innerHTML +=    `<div class="col-sm-8">
                                                                     <div class="card">
                                                                         <img class="card-img-top" src="${camera.imageUrl}" width="250" height="250" alt="zurss">
                                                                         <div class="card-body bgc-primary">
@@ -49,7 +43,12 @@ fetch(url)
                                                                     </div>       
                                                                 </div>` ; 
 
-   })
+   }})
+}
+
+
+   
+   
 
   
 
