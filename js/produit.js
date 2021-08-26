789a22640d0ce74dcad39aa278926431ca66c399
Rosine <kabore.rosine75@gmail.com>
2021-08-26 14:27:47 +0200

    modifs produit

--- js/produit.js
+++ js/produit.js
@@ -1,7 +1,7 @@
 (async function() {
   const cameraId = getCameraId();
   const camera = await showCamera(cameraId);
-})()
+})
 
 //récupération de l'id du produit
 function getCameraId() {
@@ -21,11 +21,11 @@ function showCamera(cameraId) {
                                                                             <h3 class="card-title black">${camera.name}</h3>
                                                                             <h4 class="card-price black">${camera.price / 100} €</h4>
                                                                             <label for="choice">Choisissez une option</label>
-                                                                            <select name="option_lense" class="lenses">
+                                                                            <select name="option_lense" id="option_lense" class="lenses">
                                                                              
                                                                             </select>
                                                                             <label for="quantity">Quantité</label>
-                                                                            <select name="quantity-product">
+                                                                            <select id="quantity-product" name="quantity-product">
                                                                               <option>1</option>
                                                                               <option>2</option>
                                                                               <option>3</option>
@@ -52,56 +52,60 @@ function showCamera(cameraId) {
     btnPanier.addEventListener("click", (event)=> {
      event.preventDefault();
 
-     //sélection des options
-     const selectLenses = document.querySelector("#option_lense");
+       //sélection des options
+       const selectLenses = document.querySelector("#option_lense");
 
-    //Le choix de l'utilisateur
-    const choixLenses = selectLenses.Value; 
-    console.log(choixLenses);
+       //Le choix de l'utilisateur
+       const choixLenses = selectLenses.Value; 
+       console.log(choixLenses);
 
-   //Récupération du choix de l'utilisateur
-   let produitSelection = {
-     nom: camera.name,
-     id: camera._id,
-     option: selectLenses,
-     quantite: quantiteProduit,
-     prix: camera.price / 100,
-    }
+       //Le choix d'une quantité
+      let quantiteProduit = document.querySelector("#quantity-product"); 
 
- console.log(produitSelection);
+      //Récupération du choix de l'utilisateur
+      let produitSelection = {
+           nom: camera.name,
+           id: camera._id,
+           option: selectLenses,
+           quantite: quantiteProduit,
+           prix: camera.price / 100,
+        }
 
- //Le choix d'une quantité
- let quantiteProduit = document.queryselector("#quantity-product");
+        console.log(produitSelection);
 
- //Local storage
- let produitEnregistre = JSON.parse(localStorage.getItem("produit"));
 
- //si déja produits enregistrés
- if(produitEnregistre) {
-   produitEnregistre.push(produitSelection);
-   localStorage.setItem("produit", JSON.stringify()) 
-   console.log(produitEnregistre);
-   fenetreConfirmation();
- }
- //si panier vide
- else{
-   produitEnregistre = [];
-   produitEnregistre.push(produitSelection);
-   localStorage.setItem("produit", JSON.stringify()) 
-   console.log(produitEnregistre);
-   fenetreConfirmation();
- }
+       console.log(localStorage.getItem("produit"));
+       //Local storage
+       let produitEnregistre = JSON.parse(localStorage.getItem("produit"));
+ 
 
- //fenêtre de confirmation et retour à l'accueil ou aller au panier
- const fenetreConfirmation = () =>{
-   if(window.confirm(`${camera.name} option: ${itemOption} a bien été ajouté au panier
-    Appuyez sur OK pour consulter le panier ou sur ANNULER pour revenir à la page d'accueil`)) {
-    window.location.href ="panier.html";
-   }
-   else{
-     window.location.href ="index.html";
-   }
-  }
+       //fenêtre de confirmation et retour à l'accueil ou aller au panier
+       const fenetreConfirmation = () =>{
+           if(window.confirm(`${camera.name} option: ${itemOption} a bien été ajouté au panier
+           Appuyez sur OK pour consulter le panier ou sur ANNULER pour revenir à la page d'accueil`)) {
+               window.location.href ="panier.html";
+           }
+           else{
+               window.location.href ="index.html";
+           }
+        }
+
+       //si déja produits enregistrés
+      if(produitEnregistre) {
+           produitEnregistre.push(produitSelection);
+           localStorage.setItem("produit", JSON.stringify()) 
+           console.log(produitEnregistre);
+          fenetreConfirmation();
+        }
+
+       //si panier vide
+      else{
+           produitEnregistre = [];
+           produitEnregistre.push(produitSelection);
+           localStorage.setItem("produit", JSON.stringify()) 
+           console.log(produitEnregistre);
+           fenetreConfirmation();
+        }
    
 });
   })
@@ -110,8 +114,8 @@ function showCamera(cameraId) {
 //récupération des options
 function addProductOption(jsonCamera) {
   for(let itemOption of jsonCamera.lenses) {
-    document.querySelector(".lenses").innerHTML += `<option>${itemOption}</option>`;
-  }
+     document.querySelector(".lenses").innerHTML += `<option>${itemOption}</option>`;
+    }
 }
 
 //ajouter l'article au panier
@@ -122,52 +126,38 @@ const addPanier = (productData, idElem) =>{
 
   //si le panier est vide
   if (sessionStorage.getItem('produit') === null)
-  {
+    {
       arrayCart = [{value: document.getElementById(idElem).value, id: productData._id}]
       sessionStorage.setItem('produit', JSON.stringify(arrayCart));
       sessionStorage.setItem('prixTotal', productData.price);
-  }
+    }
 
   //si le panier n'est pas vide
   else
-  {   
+    {   
       arrayCart = JSON.parse(sessionStorage.getItem('produit'));
 
       //Vérifier si le prdouit est déjà ajouté au panier ou pas
       for(let product of arrayCart)
-      {
+        {
           if(product.id === productData._id){
               isAdded = true;
-          }
-      }
+            }
+        }
 
       //ajouter le produit au panier s'il ne l'est pas
-      if(!isAdded)
-      {
+      if(!isAdded) {
           arrayCart.push(({
-              value: document.getElementById(idElem).value,
+             value: document.getElementById(idElem).value,
               id: productData._id
-              }));
-              sessionStorage.setItem('produit', JSON.stringify(arrayCart));
+            }));
+          sessionStorage.setItem('produit', JSON.stringify(arrayCart));
 
           let prixActu =  parseFloat(sessionStorage.getItem('prixTotal')) + productData.price;
           sessionStorage.setItem('prixTotal', prixActu);
-
-      }
-
-  }
-
-  
+        }
+    }
 }
 
-
-
-
-
-
-
-  
-
+   //comportement du panier quand il est vide
  
-  
-
